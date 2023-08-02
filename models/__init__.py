5c7a0401d9a3fc9ecab022a422d08577fd10f3ae
Omer Farooq <omr.frq1999@gmail.com>
2023-08-02 18:57:48 +0500

    added task model

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-from .job import Job 
+from .job import Job, Task
 
--- models/job.py
+++ models/job.py
@@ -9,14 +9,36 @@ class Job(db.Model):
     created_at = db.Column(db.DateTime, nullable = False)
     status = db.Column(db.String(10), nullable = False)
 
+    tasks = db.relationship('Task', backref='job', lazy=True)
+
     def __repr__(self):
         return f"Job(name = {name}, created_at = {created_at}, status= {status})"
+    
+#A Job can have multiple tasks associated with it, this can represent the 1 to Many Relationship
+class Task(db.Model):
+    __tablename__ = 'task'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=True)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=True)
+
+    def __repr__(self):
+        return f"Task(description={description})"
+
 
 class JobSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Job
         load_instance = True
 
+
+class TaskSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Task
+        load_instance = True
+
 job_schema = JobSchema()
 jobs_schema = JobSchema(many=True)
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
 
