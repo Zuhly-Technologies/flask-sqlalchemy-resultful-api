372dff43a20c28f7ed67ea970e391d35fda79293
Omer Farooq <omr.frq1999@gmail.com>
2023-08-03 19:03:58 +0500

    minor alterations

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -7,5 +8,6 @@ ma = Marshmallow()
 def initialize(app):
     db.init_app(app)
     ma.init_app(app)
-  
+
+    
 
