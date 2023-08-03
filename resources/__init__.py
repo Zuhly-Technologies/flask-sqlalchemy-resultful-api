73e988d28933e036216f72ccff2a43815f6ab658
Omer Farooq <omr.frq1999@gmail.com>
2023-08-03 19:04:22 +0500

    minor alterations

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,5 +1,8 @@
 from resources.job import JobResource, JobPostResource
+from resources.task import TaskResource, TaskPostResource
 
 def add_resource(api):
     api.add_resource(JobResource, "/job/<int:id>")
     api.add_resource(JobPostResource, "/job")
+    api.add_resource(TaskResource, "/task/<int:id>")
+    api.add_resource(TaskPostResource, "/task")
