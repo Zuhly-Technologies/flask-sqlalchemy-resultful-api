1009374d2ecf5b8c1a2405a850cb6f9bed39f8f1
Omer Farooq <omr.frq1999@gmail.com>
2023-08-03 18:56:04 +0500

    minor alterations

--- models/job.py
+++ models/job.py
@@ -5,11 +5,11 @@ class Job(db.Model):
     __tablename__ = 'job'
 
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False)
-    created_at = db.Column(db.DateTime, nullable = False)
-    status = db.Column(db.String(10), nullable = False)
+    name = db.Column(db.String(100), nullable = True)
+    created_at = db.Column(db.DateTime, nullable = True)
+    status = db.Column(db.String(10), nullable = True)
 
-
+    #tasks: db.Mapped[db.List["Task"]] = db.relationship()
     tasks = db.relationship('Task', backref='job', lazy=True)
 
     def __repr__(self):
